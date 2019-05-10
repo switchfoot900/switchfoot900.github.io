1138243930346f4f7ed8bb26020ef2c0b936636e
switchfoot900 <lop18014@byui.edu>
2019-05-09 23:30:18 -0600

    second try on first hoopla

---
+++ js/currentdate.js
@@ -0,0 +1,5 @@
+// Get Current Date
+let today = new Date(); // new Date object
+// now concatenate formatted output
+let date = (today.getMonth()+1) + " / " +  today.getDate() + " / " + today.getFullYear();
+document.getElementById('currentdate').innerHTML = date;
